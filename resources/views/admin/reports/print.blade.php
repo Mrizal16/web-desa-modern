f3e992e9e51e23e31dc45cbc26bdab3c3bca1371
Rizal <s2708jac@gmail.com>
2026-09-15 21:07:30 +0700

    Initial commit

--- resources/views/admin/reports/print.blade.php
+++ resources/views/admin/reports/print.blade.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="id">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Laporan Pelayanan Desa Sidorejo</title>
 
     <style>
